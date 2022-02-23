b81888afaa95c131c7b1bd120784f8b984a80f8c
Koston Zhuang <koston.zhuang@maiscrm.com>
2022-02-23 10:52:37 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM phusion/baseimage:master
+
+CMD ["/sbin/my_init"]
+
+RUN apt-get update && \
+  apt-get install -y git && \
+  apt-get install -y protobuf-compiler && \
+  apt-get install -y wget && \
+  wget -q https://mirrors.ustc.edu.cn/golang/go1.16.5.linux-amd64.tar.gz -O golang.tar.gz && \
+  tar -C /usr/local -xf golang.tar.gz && \
+  rm golang.tar.gz
+
+ENV GOPATH /app
+ENV GOROOT /usr/local/go
+ENV PATH ${GOPATH}/bin:${GOROOT}/bin:$PATH
+ENV GO111MODULE auto
+ENV GOPROXY https://goproxy.cn,direct
+
+WORKDIR ${GOPATH}
+
+RUN mkdir -p ${GOPATH}/src ${GOPATH}/bin && \
+    chmod -R 777 ${GOPATH} && \
+    go install google.golang.org/protobuf/cmd/protoc-gen-go@latest && \
+    go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest && \
+    go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest && \
+    go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
+
+# 在基础镜像 /etc/my_init.d/ 中的脚本会在容器启动时执行
+COPY gen.sh /etc/my_init.d/gen.sh
+
+RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
---
+++ gen.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+protoc -I ./proto/ --go_out=proto \
+        --go-grpc_out=proto \
+        --go_opt paths=source_relative \
+        --go-grpc_opt paths=source_relative proto/*.proto
+
+if test -f proto/service.yml
+then
+    protoc -I ./proto/ --go_out=proto \
+    --go-grpc_out=proto \
+    --go_opt paths=source_relative \
+    --go-grpc_opt paths=source_relative \
+    --grpc-gateway_out=proto \
+    --grpc-gateway_opt logtostderr=true \
+    --grpc-gateway_opt paths=source_relative \
+    --grpc-gateway_opt grpc_api_configuration=proto/service.yml \
+    --openapiv2_opt grpc_api_configuration=proto/service.yml \
+    --openapiv2_out=proto proto/*.proto
+fi
+
+for file in proto/*
+do
+    if test -f $file
+    then
+        echo skip $file
+    else
+        protoc -I ./proto/ --go_out=proto \
+        --go-grpc_out=proto \
+        --go_opt paths=source_relative \
+        --go-grpc_opt paths=source_relative $file/*.proto
+
+        if test -f $file/service.yml
+        then
+            protoc -I ./proto/ --go_out=proto \
+            --go-grpc_out=proto \
+            --go_opt paths=source_relative \
+            --go-grpc_opt paths=source_relative \
+            --grpc-gateway_out=proto \
+            --grpc-gateway_opt logtostderr=true \
+            --grpc-gateway_opt paths=source_relative \
+            --grpc-gateway_opt grpc_api_configuration=$file/service.yml \
+            --openapiv2_opt grpc_api_configuration=$file/service.yml \
+            --openapiv2_out=proto $file/*.proto
+        fi
+    fi
+done
+
+echo "generated all stubs"
